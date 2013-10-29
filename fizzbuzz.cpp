c9872db44de4c027a089d50b1840279195fbf3b8
Aman Agarwal <amanaamazing@gmail.com>
2013-10-29 16:59:30 -0700

    Initial Commit

---
+++ fizzbuzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  for (int i = 1; i < 101; i++) {
+    string s = "";
+    if (i % 3 == 0) {
+      s += "Fizz";
+    }
+    if (i % 5 == 0) {
+      s += "Buzz";
+    }
+    s.length() == 0 ? cout << i << endl : cout << s << endl;
+  }
+}
