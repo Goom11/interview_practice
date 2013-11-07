215969d5874f8dbf466ab67f7b276e9b44ba4880
Aman Agarwal <amanaamazing@gmail.com>
2013-11-07 10:43:27 -0800

    use 'from collections import defaultdict'

--- isUniqueChars/isUniqueChars.py
+++ isUniqueChars/isUniqueChars.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
+from collections import defaultdict
+
 def isUniqueChars(s):
-  char_set = [False] * 256
+  char_set = defaultdict(bool)
   for char in s:
-    val = ord(char)
-    if (char_set[val]):
+    if (char_set[char]):
       return False
-    char_set[val] = True
+    char_set[char] = True
   return True
 
 str1 = "SO NDUPE"
