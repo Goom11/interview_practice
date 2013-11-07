9a7fe4422ea40b76049b486aae0899c338558a60
Aman Agarwal <amanaamazing@gmail.com>
2013-11-07 12:33:47 -0800

    python isRotation, uses string.find(sub)

---
+++ isRotation/isRotation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def isRotation(str1, str2):
+  length = len(str1)
+  if length == len(str2) and length > 0:
+    str1str1 = str1 + str1
+    found = str1str1.find(str2)
+    if found != -1:
+      return True
+  return False
+
+print isRotation("abc", "bca")
