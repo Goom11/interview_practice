12de115f979d7c468fa399502b8521045f217fd0
Aman Agarwal <amanaamazing@gmail.com>
2013-11-07 00:11:37 -0800

    Reverse cpp

---
+++ reverse/reverse.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void swap(char* a, char* b) {
+  char tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+void reverse(char* str) {
+  char* end = str;
+  if (str) {
+    while (*end) {
+      end++;
+    }
+    end--;
+    while (str < end) {
+      swap(str, end);
+      str++;
+      end--;
+    }
+  }
+}
+
+
+int main() {
+  char a[] = "Hello";
+  cout << a << " reversed is ";
+  reverse(a);
+  cout << a << endl;
+}
