6e76d6dacdc94dd2a5f26006a02dc13120e78a7d
Aman Agarwal <amanaamazing@gmail.com>
2013-11-15 07:42:48 -0800

    Format change to follow pep8 style guide using autopep8 tool

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -2,22 +2,23 @@
 
 from collections import defaultdict
 
+
 def anagram(a, b):
-  if (len(a) != len(b)):
-    return False
-  char_set = defaultdict(int)
-  for char in a:
-    char_set[char] += 1
-  for char in b:
-    char_set[char] -= 1
-  for char, amount in char_set.iteritems():
-    if amount != 0:
-      return False
-  return True
+    if (len(a) != len(b)):
+        return False
+    char_set = defaultdict(int)
+    for char in a:
+        char_set[char] += 1
+    for char in b:
+        char_set[char] -= 1
+    for char, amount in char_set.iteritems():
+        if amount != 0:
+            return False
+    return True
 
 a = "Hello"
 b = "olelH"
 print a, "is",
 if not anagram(a, b):
-  print "not",
+    print "not",
 print "an anagram of", b
