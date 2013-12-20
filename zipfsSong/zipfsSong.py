a74a3e768d6d84a6bb5ba9bede7a51250d90da55
Aman Agarwal <aman.agarwal.2718@gmail.com>
2013-12-20 02:06:11 -0800

    simplified quality, removed class, yay functional

--- zipfsSong/zipfsSong.py
+++ zipfsSong/zipfsSong.py
@@ -2,46 +2,35 @@
 
 from heapq import nlargest
 
-class Song(object):
-    """Simple Song class used to determine quality of songs on an album"""
+def make_song(number, plays, name):
+    return {
+        'number': number,
+        'plays': plays,
+        'name': name,
+        'quality': plays * number,
+    }
 
-    def __init__(self, number, frequency, name):
-        self.number = number
-        self.frequency = float(frequency)
-        self.name = name
-
-    # Alternate constructor
-    @classmethod
-    def from_input(cls, input, number):
-        """Constructs Song object given song number and an input in the format: '#{num_plays} #{name}'"""
-        frequency, name = input.split()
-        frequency = float(frequency)
-        return cls(number, frequency, name)
-
-    def get_quality(self, first_frequency):
-        """Gets quality based on qi = fi/zi where zi is proportional to 1/i"""
-        zipfs = first_frequency / self.number
-        return self.frequency / zipfs
+def make_song_from_string(string_input, number):
+    """Constructs song dict given song number and a string in the format:
+       '#{num_plays} #{name}'
+    """
+    frequency, name = string_input.split()
+    frequency = int(frequency)
+    return make_song(number, frequency, name)
 
 def get_top_m(songs, m):
     """Returns an array of top m songs based on quality"""
     num_songs = len(songs)
-    first_frequency = songs[0].frequency
-    # Each entry is a 3-element list containing the priority, an entry count reversed, and the song
-    pq = [[song.get_quality(first_frequency), num_songs - song.number, song] for song in songs]
+    # Each entry is a 3-element list containing the priority (quality), an entry count reversed, and the song name
+    pq = [[song['quality'], -song['number'], song['name']] for song in songs]
     # The total array is treated as a priority queue implemented using python's heapq
     return nlargest(m, pq)
 
 def run():
     n, m = map(int, raw_input().split())
-    songs = [Song.from_input(raw_input(), i + 1) for i in xrange(n)]
+    songs = [make_song_from_string(raw_input(), i + 1) for i in xrange(n)]
     top_m = get_top_m(songs, m)
-    print '\n'.join([entry[2].name for entry in top_m])
+    print '\n'.join([entry[2] for entry in top_m])
 
-run()
-
-# s1 = Song(1, 100, "a")
-# s2 = Song(2, 50, "b")
-# s3 = Song(3, 100, "c")
-# songs = [s1, s2, s3]
-# t = get_top_m(songs, 3)
+if __name__ == "__main__":
+    run()
