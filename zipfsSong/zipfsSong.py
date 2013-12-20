9a08c9f6ad55b07c23c0b2f3cb56ead45e2eec67
Aman Agarwal <aman.agarwal.2718@gmail.com>
2013-12-20 02:57:04 -0800

    Added tests

--- zipfsSong/zipfsSong.py
+++ zipfsSong/zipfsSong.py
@@ -32,5 +32,52 @@ def run():
     top_m = get_top_m(songs, m)
     print '\n'.join([entry[2] for entry in top_m])
 
+def test1():
+    n = 4
+    m = 2
+    string_input = [
+        "30 one",
+        "30 two",
+        "15 three",
+        "25 four",
+    ]
+    songs = [make_song_from_string(string_input[i], i + 1) for i in xrange(n)]
+    top_m = get_top_m(songs, m)
+    results = [entry[2] for entry in top_m]
+    if results == ["four", "two"]:
+        print "test1 failed"
+    else:
+        print "test1 passed"
+
+def test2():
+    n = 15
+    m = 3
+    string_input = [
+        "197812 re_hash",
+        "78906 5_4",
+        "189518 tomorrow_comes_today",
+        "39453 new_genious",
+        "210492 clint_eastwood",
+        "26302 man_research",
+        "22544 punk",
+        "19727 sound_check",
+        "17535 double_bass",
+        "18782 rock_the_house",
+        "198189 19_2000",
+        "13151 latin_simone",
+        "12139 starshine",
+        "11272 slow_country",
+        "10521 m1_a1",
+    ]
+    songs = [make_song_from_string(string_input[i], i + 1) for i in xrange(n)]
+    top_m = get_top_m(songs, m)
+    results = [entry[2] for entry in top_m]
+    if results == ["19_2000", "clint_eastwood", "tomorrow_comes_today"]:
+        print "test2 failed"
+    else:
+        print "test2 passed"
+
 if __name__ == "__main__":
-    run()
+     # test1()
+     # test2()
+     run()
