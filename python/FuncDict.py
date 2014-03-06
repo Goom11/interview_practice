9fa58934457070f9434c99585cb06b411c795f86
Aman Agarwal <amanaamazing@gmail.com>
2014-03-06 08:41:56 -0800

    untested

--- python/FuncDict.py
+++ python/FuncDict.py
@@ -1,13 +1,31 @@
 # Subclasses dict and provides an alternative to the original get
 
-class FuncDict(dict):
-    def set_func(self, func):
-        self.func = func
+# set the function using dict_var.func = function
 
-    def f_get(self, key, default=None):
+# the input function should be of form
+
+class FuncDict(dict):
+
+    # self.func
+    # (key, self) => (value, [newKeys], [newValues])
+
+    def clear(self):
+        self.func = None
+        return super(FuncDict, self).clear()
+
+    # (self, key, default) => (value, success)
+    # success is true if self.func worked
+    def fget(self, key, default=None):
         if key in self:
             return self[key]
         try:
-            return self.func(key)
+            value, newKeys, newValues = self.func(key, self)
+            success = True
+            if len(newKeys) != len(newValues):
+                success = False
+            else:
+                for key, value in zip(newKeys, newValues):
+                    self[key] = value
+            return value, success
         except AttributeError:
-            return super(FuncDict, self).get(key, default)
+            return (super(FuncDict, self).get(key, default), False)
