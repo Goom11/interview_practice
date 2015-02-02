720fdcab851265571ad75469c5d6eeeb78dc5a18
Aman Agarwal <amanaamazing@gmail.com>
2015-02-02 00:21:31 -0800

    I traced through it and now it makes sense.

--- matchStringPattern/stringmatch.pl
+++ matchStringPattern/stringmatch.pl
@@ -4,14 +4,17 @@ match(Pattern, String, OutputMapping) :-
 
 match_rec([], '', Mapping, Mapping).
 
+% This predicate is for when the next symbol already exists in our mapping
 match_rec([Symbol|RestPattern], String, InputMapping, OutputMapping) :-
     get_assoc(Symbol, InputMapping, MatchedString),
-    !,
+    !, % This prevents the program from matching a symbol in out mapping and
+       % not using it.
     atom_length(MatchedString, Length),
     sub_atom(String, 0, Length, RestLength, MatchedString),
     sub_atom(String, Length, RestLength, _, RestString),
     match_rec(RestPattern, RestString, InputMapping, OutputMapping).
 
+% This predicate tries matching all substrings to the symbol
 match_rec([Symbol|RestPattern], String, InputMapping, OutputMapping) :-
     atom_length(String, TotalLength),
     sub_atom(String, 0, Length, RestLength, MatchedString),
